064ac4c5480670d6ee36fd9bfe8581718c8c00d8
SoyRober <roberlamilagrosa@gmail.com>
2023-06-09 09:47:06 +0200

    Falta arreglar los barcos y disparos

--- index.js
+++ index.js
@@ -12,20 +12,8 @@ const nFilas = 11;
 const anchoCasilla = canvas1.width / nFilas;
 const alturaCasilla = canvas1.height / nColumnas;
 
-function cargar() {
-    fetch(url)
-    .then(response => {
-
-        if (response.ok) return response.json();
-        else{
-            alert("No s'ha pogut carregar. Error: " + response.status)
-        }
-    })
-
-    .then(data => {
-        pintarBarcos(data);
-    });
-}
+const letras = ['', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+var barcos = [];
 
 function dibujarCanvas(contexto, canvas) {
     //Dejamos el canavas con fondo blanco de manera predeterminada
@@ -40,43 +28,128 @@ function dibujarCanvas(contexto, canvas) {
         }
     }
 
+    pintarLetrasYNumeros(contexto);
+}
+
+function pintarLetrasYNumeros(contexto) {
+    contexto.fillStyle = "black";
+    contexto.font = "12px Arial";
+
     for (let fila = 0; fila < nColumnas; fila++) {
         for (let col = 0; col < nFilas; col++) {
             contexto.strokeRect(col * anchoCasilla, fila * alturaCasilla, anchoCasilla, alturaCasilla);
-
+            if (col === 0) {
+                contexto.textAlign = "center";
+                contexto.fillText(letras[fila], anchoCasilla / 2, fila * alturaCasilla + alturaCasilla / 2);
+            }
+        }
+        if (fila === 0) {
+            for (let col = 1; col < nFilas; col++) {
+                contexto.textAlign = "center";
+                contexto.fillText(col, col * anchoCasilla + anchoCasilla / 2, alturaCasilla / 2);
+            }
         }
     }
-
-}
-
-function rellenarCelda(contexto, fila, col, color, texto) {
-    contexto.fillStyle = color;
-    const x = col * anchoCasilla;
-    const y = fila * alturaCasilla;
-    contexto.fillRect(y, x, anchoCasilla, alturaCasilla);
 }
 
 function inicio() {
     //Dibujamos el canvas de los dos jugadores
     dibujarCanvas(contexto1, canvas1);
     dibujarCanvas(contexto2, canvas2);
+    cargarBarcos();
 }
 
-canvas2.addEventListener('click', function(evento){
-    const x = evento.clientX - canvas2.offsetLeft; 
-    const y = evento.clientY - canvas2.offsetTop;
+canvas2.addEventListener('click', marcar);
 
-    const columna = Math.floor(x/anchoCasilla);
-    const fila = Math.floor(y/alturaCasilla);
+function marcar(event) {
+    const x = event.clientX - canvas2.offsetLeft;
+    const y = event.clientY - canvas2.offsetTop;
+  
+    const fila = Math.floor(y / alturaCasilla);
+    const columna = Math.floor(x / anchoCasilla);
 
     if (fila > 0 && columna > 0) {
-        
+        disparo(contexto2, fila, columna, 'red');
     }
+  
+}
 
-});
+function disparo(contexto, fila, columna, color) {
+    const x = fila * anchoCasilla; 
+    const y = columna * alturaCasilla;
+  
+    contexto.fillStyle = color;
+    contexto.fillRect(y, x, anchoCasilla, alturaCasilla);
+}
 
-function pintarBarcos(barco) {
-    barco.forEach(element => {
-        console.log(element)
+function rellenarCelda(contexto, fila, col, color) {
+    contexto.fillStyle = color;
+    const x = col * anchoCasilla;
+    const y = fila * alturaCasilla;
+    contexto.fillRect(y, x, anchoCasilla, alturaCasilla);
+}
+
+function cargarBarcos() {
+    fetch('barcos.json')
+    .then(response => response.json())
+    .then(data => {
+        colocarBarcos(contexto1, data)
+    })
+
+}
+
+function colocarBarcos(contexto, data) {
+    barcos = rellenarArray(barcos);
+
+    data.barcos.forEach(element => {
+        const xNueva = Math.floor(Math.random()*11);
+        const yNueva = Math.floor(Math.random()*11);
+        const orientacion = (Math.floor(Math.random()*2) % 2 == 0)? "H" : "V";
+
+        element.x = xNueva;
+        element.y = yNueva;
+        element.orientacion = orientacion;
+
+        dibujarBarco(contexto, element.x, element.y, element.orientacion, element.vida);
+        console.log(barcos);
     });
+}
+
+function dibujarBarco(contexto, x, y, orientacion, tamaño) {
+    const color = 'blue'; 
+
+    const xInicial = y * anchoCasilla;
+    const yInicial = x * alturaCasilla;
+
+    if (orientacion === 'H') {
+        for (let i = 0; i < tamaño; i++) {
+            const xActual = xInicial + i * anchoCasilla;
+            contexto.fillStyle = color;
+            contexto.fillRect(xActual, yInicial, anchoCasilla, alturaCasilla);
+            barcos[x+i][y] = 'B'
+        }
+    } else if (orientacion === 'V') {
+        for (let i = 0; i < tamaño; i++) {
+            const yActual = yInicial + i * alturaCasilla;
+            contexto.fillStyle = color;
+            contexto.fillRect(xInicial, yActual, anchoCasilla, alturaCasilla);
+            barcos[x][y+i] = 'B'
+        }
+    }
+}
+
+function rellenarArray(barcos) {
+
+    var filas = 10;
+    var columnas = 10;
+    
+    for (let fila = 0; fila < filas; fila++) {
+        barcos[fila] = [];
+    
+        for (let columna = 0; columna < columnas; columna++) {
+            barcos[fila][columna] = ""; 
+        }
+    }
+    
+    return barcos;
 }
